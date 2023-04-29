7d424fa7d502b3d574e32f813e33d58fd9455846
navin chhorn <navin.chhorn09012004@gmail.com>
2023-04-29 18:50:26 +0700

    [10]: create method for user story 6

--- src/Plane/Gate.ts
+++ src/Plane/Gate.ts
@@ -2,14 +2,13 @@ import { Flight } from "../Flight/Flight";
 
 export class Gate{
     private gateNumber:string;
-    private flights : Flight [] = [];
-    constructor(gateNumber:string,filght : Flight){
+    constructor(gateNumber:string){
         this.gateNumber = gateNumber;
         
     }
 
-    addFightToGate(fight: Flight){
-       return this.flights.push(fight);
+    getGateNumber(){
+        return this.gateNumber;
     }
     
 }
